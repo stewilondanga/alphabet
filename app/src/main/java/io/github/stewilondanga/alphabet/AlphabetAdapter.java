2a68bffeca7cd3e446dc1acb977e7d70b2d9d71f
Stewart ilondanga <stilondanga@gmail.com>
2018-06-06 16:18:34 +0300

    add app/src/main/res/layout/activity_main.xml

--- app/src/main/java/io/github/stewilondanga/alphabet/AlphabetAdapter.java
+++ app/src/main/java/io/github/stewilondanga/alphabet/AlphabetAdapter.java
@@ -14,50 +14,46 @@ import android.widget.TextView;
 
 public class AlphabetAdapter extends BaseAdapter {
     private Context mContext;
-    private String[] mletters;
+    private String[] mLetters;
     private Typeface mTypeface;
 
-    public AlphabetAdapter (Context context, String [] letters) {
+    public AlphabetAdapter(Context context, String[] letters, Typeface typeface ) {
         this.mContext = context;
-        this.mletters = letters;
+        this.mLetters = letters;
         this.mTypeface = typeface;
     }
 
-    @Override
-    public int getCount() {
-        return mletters.length;
-    }
 
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
+@Override
+public int getCount() {
+    return mLetters.length;
+}
+@Override
+public Object getItem(int position) {
+   return null;
+}
+@Override
+public long getItemId(int position) {
+   return 0;
+}
 
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) mContext
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View gridView;
-        if (convertView == null) {
-            //get layout from xml file
-            gridView = inflater.inflate(R.layout.alphabet_grid_item, null);
-                // pull views
-            TextView letterView = (TextView) gridView
+@Override
+public View getView(int position, View convertView, ViewGroup parent) {
+   LayoutInflater inflater = (LayoutInflater) mContext
+           .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+   View gridView;
+   if (convertView == null) {
+       //get layout from xml file
+       gridView = inflater.inflate(R.layout.alphabet_grid_item, null);
+           // pull views
+           TextView letterView = (TextView) gridView
                     .findViewById(R.id.grid_item_letter);
-            //set values into views
-            letterView.setText("mLetters[position]");
-            letterView.setTypeface(mTypeface);
-        } else {
+           //set values into views
+           letterView.setText("mLetters[position]");
+           letterView.setTypeface(mTypeface);
+       } else {
             gridView = (View) convertView;
-        }
-        return gridView;
-    }
-
-
-
+       }
+       return gridView;
+   }
 }
--- app/src/main/java/io/github/stewilondanga/alphabet/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/alphabet/MainActivity.java
@@ -14,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Kingthings_Calligraphica_Light.ttf");
+        gridView = (GridView) findViewById(R.id.baseGridView);
+        gridView.setAdapter(new AlphabetAdapter(this, letters, typeface));
     }
 }
